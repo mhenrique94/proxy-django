fe85c696cda5e4026ca6841bd280844818c5ec9f
Marcelo Silva <mhenrique94@outlook.com>
2023-07-23 00:10:59 -0300

    implements error response

--- core/views.py
+++ core/views.py
@@ -1,8 +1,12 @@
+from http import HTTPStatus
 from django.http import JsonResponse
 
 from core.main_proxy_svc import get_proxies_from_freeapiproxies
 
 
 def get_proxies(request):
-    proxy_list, raw_data = get_proxies_from_freeapiproxies()
+    try:
+        proxy_list, raw_data = get_proxies_from_freeapiproxies()
+    except Exception:
+        return JsonResponse(status=HTTPStatus.SERVICE_UNAVAILABLE)
     return JsonResponse({ "proxy_list": proxy_list, "raw_data": raw_data })
