7ff994a8f3b1bee6a61dc47f88b5e085d270e732
Marcelo Silva <mhenrique94@outlook.com>
2023-07-23 16:28:54 -0300

    adds BASE_DIR info

--- proxydjango/settings.py
+++ proxydjango/settings.py
@@ -18,7 +18,7 @@ import dj_database_url
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
