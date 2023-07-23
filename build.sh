c1d1ace29159c25044aad96327ccb228b9f4b895
Marcelo Silva <mhenrique94@outlook.com>
2023-07-23 16:41:55 -0300

    ops

--- build.sh
+++ build.sh
@@ -6,5 +6,3 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-postgres://proxy_django_user:Y5qmJKRYyimBSTcvIlx2Pbgshdc5bfo9@dpg-ciunpmtgkuvoig948t3g-a/proxy_django
